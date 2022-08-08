6d7622ab76e56eaf8b5094444ac6a092ed2f67ff
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-08 13:54:36 +0530

    Added steps to stop gambling if got loss in current month

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -8,48 +8,62 @@ public class Gambler {
 	public static final int NUM_OF_DAY_PLAYED_IN_MONTH = 20;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome Gambling Stimulation Problem");
-		int investedAmount = NUM_OF_DAY_PLAYED_IN_MONTH*PLAYER_BALANCE_PER_DAY;
-		int totalEarnedOrLost = 0;
-		int day = 1;
-		int profitdays = 0;
-		int lossdays = 0;
-		while(day<=NUM_OF_DAY_PLAYED_IN_MONTH ) {
-			int playerCurrentBalance = PLAYER_BALANCE_PER_DAY ;
-			//System.out.println("Day "+day);	
-			while(playerCurrentBalance>50&&playerCurrentBalance<150) {
-				//Choice value 0 means lost current game , 1 means won current bet
-				int choice = (int)(Math.random()*10 %2);
-				switch(choice){
-					case WON_CURRENT_BET:
-						playerCurrentBalance++;
-						break;
-					case LOST_CURRENT_BET:
-						playerCurrentBalance--;
-						break;
+		System.out.println("Welcome Gambling Stimulation Problem\n");
+		int loss ;
+		int profit=0;
+		int month =1, monthlyEarning = 0;
+		while(month<12) {
+			int investedAmount = NUM_OF_DAY_PLAYED_IN_MONTH*PLAYER_BALANCE_PER_DAY;
+			int totalEarnedOrLost = 0;
+			int day = 1;
+			int profitdays = 0;
+			int lossdays = 0;
+			while(day<=NUM_OF_DAY_PLAYED_IN_MONTH ) {
+				int playerCurrentBalance = PLAYER_BALANCE_PER_DAY ;
+				//System.out.println("Day "+day);	
+				while(playerCurrentBalance>50&&playerCurrentBalance<150) {
+					//Choice value 0 means lost current game , 1 means won current bet
+					int choice = (int)(Math.random()*10 %2);
+					switch(choice){
+						case WON_CURRENT_BET:
+							playerCurrentBalance++;
+							break;
+						case LOST_CURRENT_BET:
+							playerCurrentBalance--;
+							break;
+					}
+					//System.out.println(playerCurrentBalance);
 				}
-				//System.out.println(playerCurrentBalance);
+				if(playerCurrentBalance==50) {
+					lossdays++;
+				}
+				else {
+					profitdays++;
+				}
+				totalEarnedOrLost = totalEarnedOrLost + playerCurrentBalance;
+				//System.out.println("Day "+day+"  Balance "+playerCurrentBalance);
+				day++;
 			}
-			if(playerCurrentBalance==50) {
-				lossdays++;
+			//Display Per Month Earnings
+			System.out.println("Month :"+month+" Details");
+			System.out.println("In this month you won for "+profitdays+" days and lost for "+lossdays);
+			month++;	
+			//Display Monthly Earnings
+			if(investedAmount<=totalEarnedOrLost) {
+				profit = totalEarnedOrLost - investedAmount;
+				System.out.println("You earned "+profit+" in this month");
+				monthlyEarning = monthlyEarning + profit;
+				//System.out.println("Amount Earned So far: "+monthlyEarning );
 			}
-			else {
-				profitdays++;
+			//Display Monthly Loss
+			if(investedAmount>totalEarnedOrLost) {
+				loss = investedAmount - totalEarnedOrLost;
+				System.out.println("You lost "+loss+" in this month");
+				monthlyEarning = monthlyEarning - loss;
+				System.out.println("Amount Earned or Lost So far: "+monthlyEarning );
+				break;
 			}
-			totalEarnedOrLost = totalEarnedOrLost + playerCurrentBalance;
-			System.out.println("Day "+day+"  Balance "+playerCurrentBalance);
-			day++;
-		}
-		System.out.println("In this month you won for "+profitdays+" days and lost for "+lossdays);		
-		//Display Monthly Earnings
-		if(investedAmount<=totalEarnedOrLost) {
-			int profit = totalEarnedOrLost - investedAmount;
-			System.out.println("You earned "+profit+" in this month");
-		}
-		//Display Monthly Loss
-		if(investedAmount>totalEarnedOrLost) {
-			int loss = investedAmount - totalEarnedOrLost;
-			System.out.println("You lost "+loss+" in this month");
-		}
+			System.out.println();
+		}	
 	}
 }
